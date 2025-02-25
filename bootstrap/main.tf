b763775abb0b3e2be4df6eea71779557b02292a5
Igor Gorovyy <igorgorovoy@ukr.net>
2025-02-25 13:44:48 +0100

    test checks. i8

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -78,7 +78,7 @@ resource "helm_release" "kbot_app" {
 }
 # Iterator for testing version v1.0.0 
 resource "null_resource" "test" {
-  count = 10
+  count = 1
   triggers = {
     test = count.index
   }
